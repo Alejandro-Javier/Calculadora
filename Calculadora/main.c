cc05c7d2db5550e8a290b61a3fdc62650321f8a8
el_muly <el_muly@el_muly-PC>
2017-11-30 20:36:31 +0100

    Calculadora

---
+++ Calculadora/main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: el_muly
+ *
+ * Created on 30 de noviembre de 2017, 20:33
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
